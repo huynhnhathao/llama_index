c1f91463cf175d304a112b622ee3d0f39064419a
huynhnhathao <kzdmxq@gmail.com>
2025-07-11 17:38:08 +0700

    update vector_stores's node_to_metadata_dict function to exclude unset and default fields because it is creating too much unnecessary data in metadatas for chroma db

--- llama-index-core/llama_index/core/vector_stores/utils.py
+++ llama-index-core/llama_index/core/vector_stores/utils.py
@@ -41,7 +41,7 @@ def node_to_metadata_dict(
     """Common logic for saving Node data into metadata dict."""
     # Using mode="json" here because BaseNode may have fields of type bytes (e.g. images in ImageBlock),
     # which would cause serialization issues.
-    node_dict = node.model_dump(mode="json")
+    node_dict = node.model_dump(mode="json", exclude_defaults=True, exclude_none=True, exclude_unset=True)
     metadata: Dict[str, Any] = node_dict.get("metadata", {})
 
     if flat_metadata:
